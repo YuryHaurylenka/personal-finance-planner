0696d80d3eb258b70a0b0343ef09423dab2b7a98
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-05 01:43:24 +0300

    add budget_router

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.views.user import router as user_router
 from src.views.transaction import router as transaction_router
 from src.views.goal import router as goal_router
 from src.views.category import router as category_router
+from src.views.budget import router as budget_router
 
 
 @asynccontextmanager
@@ -18,6 +19,7 @@ app.include_router(user_router)
 app.include_router(transaction_router)
 app.include_router(goal_router)
 app.include_router(category_router)
+app.include_router(budget_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8000)
