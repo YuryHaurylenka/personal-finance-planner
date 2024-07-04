bc314a02a97f91d859be715b7878c9ad4d7c7570
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-04 22:06:52 +0300

    add config.py with db settings

---
+++ src/core/config.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+from sqlalchemy.orm import declarative_base
+
+BASE_DIR = Path(__file__).parent.parent.parent
+DB_PATH = BASE_DIR / "db.sqlite3"
+
+
+class Settings(BaseModel):
+    api_v1_prefix: str = "/api/v1"
+    db_url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    # edcho: bool = False
+    db_echo: bool = True
+
+
+settings = Settings()
+Base = declarative_base()
