7934a191b34dbfaeedbbfab5138419e54b95cd13
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-04 22:55:19 +0300

    change Transaction schema

--- src/schemas/transaction.py
+++ src/schemas/transaction.py
@@ -1,25 +1,33 @@
+from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
 from typing import Optional
-
-from pydantic import BaseModel, ConfigDict
+import uuid
 
 
 class TransactionBase(BaseModel):
     amount: float
-    transaction_type: str
-    category: str
+    timestamp: datetime
     description: Optional[str] = None
-    transaction_date: datetime
+    category_id: Optional[int] = None
 
 
 class TransactionCreate(TransactionBase):
+    user_id: uuid.UUID
+
+
+class TransactionUpdate(TransactionBase):
     pass
+
+
+class TransactionUpdatePartial(TransactionCreate):
+    amount: float | None = None
+    timestamp: datetime | None = None
+    description: str | None = None
+    category_id: int | None = None
+    user_id: uuid.UUID | None = None
 
 
 class Transaction(TransactionBase):
     model_config = ConfigDict(from_attributes=True)
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
+    transaction_id: int
+    user_id: uuid.UUID
