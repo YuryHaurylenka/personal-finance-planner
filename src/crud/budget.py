9bb6690fd67e90e5825e924a069042f1a0c6119c
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-05 01:42:45 +0300

    add crud for budget model

---
+++ src/crud/budget.py
@@ -0,0 +1,51 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.budget import Budget
+from src.schemas.budget import (
+    BudgetCreate,
+    BudgetUpdate,
+    BudgetUpdatePartial,
+)
+
+
+async def get_budgets(session: AsyncSession) -> list[Budget]:
+    stmt = select(Budget).order_by(Budget.budget_id)
+    result: Result = await session.execute(stmt)
+    budgets = result.scalars().all()
+    return list(budgets)
+
+
+async def get_budget(session: AsyncSession, budget_id: int) -> Budget | None:
+    return await session.get(Budget, budget_id)
+
+
+async def create_budget(
+    session: AsyncSession, category_in: BudgetCreate
+) -> Budget:
+    budget = Budget(**category_in.model_dump())
+    session.add(budget)
+    await session.commit()
+    # await session.refresh(category)
+    return budget
+
+
+async def update_budget(
+    session: AsyncSession,
+    budget: Budget,
+    budget_update: BudgetUpdate | BudgetUpdatePartial,
+    partial: bool = False,
+) -> Budget:
+    for name, value in budget_update.model_dump(exclude_unset=partial).items():
+        setattr(budget, name, value)
+    await session.commit()
+    return budget
+
+
+async def delete_budget(
+    session: AsyncSession,
+    budget: Budget,
+) -> None:
+    await session.delete(budget)
+    await session.commit()
---
+++ src/views/category.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core import db_helper
+from src.crud import category as crud_category
+from src.schemas.category import (
+    Category,
+    CategoryCreate,
+    CategoryUpdate,
+    CategoryUpdatePartial,
+)
+from src.utils.dependencies.category_dependencies import category_by_id
+
+router = APIRouter(prefix="/categories", tags=["Categories"])
+
+
+@router.get("/", response_model=list[Category])
+async def get_categories(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud_category.get_categories(session=session)
+
+
+@router.post(
+    "/",
+    response_model=Category,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_category(
+    category_in: CategoryCreate,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud_category.create_category(session=session, category_in=category_in)
+
+
+@router.get("/{category_id}/", response_model=Category)
+async def get_category(
+    category: Category = Depends(category_by_id),
+):
+    return category
+
+
+@router.put("/{category_id}/")
+async def update_category(
+    category_update: CategoryUpdate,
+    category: Category = Depends(category_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud_category.update_category(
+        session=session,
+        category=category,
+        category_update=category_update,
+    )
+
+
+@router.patch("/{category_id}/")
+async def update_category_partial(
+    category_update: CategoryUpdatePartial,
+    category: Category = Depends(category_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud_category.update_category(
+        session=session,
+        category=category,
+        category_update=category_update,
+        partial=True,
+    )
+
+
+@router.delete("/{category_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_category(
+    category: Category = Depends(category_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> None:
+    await crud_category.delete_category(session=session, category=category)
