c41a6910d860b7626e1477834c3a2529f2ffe997
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-04 22:54:49 +0300

    change Budget schema

--- src/schemas/budget.py
+++ src/schemas/budget.py
@@ -1,16 +1,33 @@
+import uuid
+from datetime import datetime
+
 from pydantic import BaseModel, ConfigDict
 
 
 class BudgetBase(BaseModel):
+    amount: float
     category: str
-    limit_amount: float
+    start_date: datetime
+    end_date: datetime
 
 
 class BudgetCreate(BudgetBase):
+    user_id: uuid.UUID
+
+
+class BudgetUpdate(BudgetBase):
     pass
+
+
+class BudgetUpdatePartial(BudgetCreate):
+    amount: float | None = None
+    category: str | None = None
+    start_date: datetime | None = None
+    end_date: datetime | None = None
+    user_id: uuid.UUID | None = None
 
 
 class Budget(BudgetBase):
     model_config = ConfigDict(from_attributes=True)
-    id: int
-    owner_id: int
+    budget_id: int
+    user_id: uuid.UUID
