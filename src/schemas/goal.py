04d57f2cda087461adb4f0626c688bafd1cd7bd4
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-04 22:54:58 +0300

    change Goal schema

--- src/schemas/goal.py
+++ src/schemas/goal.py
@@ -1,19 +1,34 @@
-from datetime import date
+import uuid
+from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
 
 
 class GoalBase(BaseModel):
+    amount: float
     description: str
-    target_amount: float
-    target_date: date
+    target_date: datetime
+    category_id: Optional[int] = None
 
 
 class GoalCreate(GoalBase):
+    user_id: uuid.UUID
+
+
+class GoalUpdate(GoalBase):
     pass
 
 
-class GoalI(GoalBase):
+class GoalUpdatePartial(GoalCreate):
+    amount: float | None = None
+    description: str | None = None
+    target_date: datetime | None = None
+    category_id: int | None = None
+    user_id: uuid.UUID | None = None
+
+
+class Goal(GoalBase):
     model_config = ConfigDict(from_attributes=True)
-    id: int
-    owner_id: int
+    goal_id: int
+    user_id: uuid.UUID
