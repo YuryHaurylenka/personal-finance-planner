ffed72d4b1e090f77d5103032367bc1c1e0a92f5
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-04 03:52:29 +0300

    add user_dependencies with user_by_id filter

---
+++ src/utils/dependencies/user_dependencies.py
@@ -0,0 +1,21 @@
+import uuid
+from typing import Annotated
+
+from fastapi import Path, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.crud import user as crud_user
+from src.database import User, db_helper
+
+
+async def user_by_id(
+    user_id: Annotated[uuid.UUID, Path],
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> User:
+    user = await crud_user.get_user(session=session, user_id=user_id)
+    if user is not None:
+        return user
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User {user_id} not found!",
+    )
