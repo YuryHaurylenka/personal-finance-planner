51bf00219e6c348c2c6a9a74245d1c560b7c9dc6
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-04 22:42:10 +0300

    add category_id to Goal and Transaction models

--- src/models/goal.py
+++ src/models/goal.py
@@ -5,6 +5,7 @@ from sqlalchemy import (
     DateTime,
     Float,
     ForeignKey,
+    Integer,
     Text,
 )
 from sqlalchemy.dialects.postgresql import UUID
@@ -18,11 +19,13 @@ class Goal(Base):
 
     goal_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.user_id"), nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.category_id"), nullable=True)
     amount = Column(Float, nullable=False)
     description = Column(Text, nullable=False)
     target_date = Column(DateTime, nullable=False)
 
     user = relationship("User", back_populates="goals")
+    category = relationship("Category", back_populates="transactions")
 
     def __repr__(self):
         return f"<Goal id={self.goal_id} amount={self.amount}>"
