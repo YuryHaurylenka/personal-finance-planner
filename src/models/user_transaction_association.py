1f38107b8227cf16001933770b0c299f1b70688a
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-06 15:27:32 +0300

    create user transaction association table

---
+++ src/models/user_transaction_association.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, Table, UniqueConstraint
+from .base import Base
+
+user_transaction_association_table = Table(
+    "user_transaction_association",
+    Base.metadata,
+    Column("user_transaction_id", Integer, primary_key=True),
+    Column("user_id", ForeignKey("users.user_id"), nullable=False),
+    Column("transaction_id", ForeignKey("transactions.transaction_id"), nullable=False),
+    UniqueConstraint(
+        "user_id", "transaction_id", name="idx_unique_user_transaction_association"
+    ),
+)
