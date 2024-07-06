2f0461fc985abd83264f5f18d1d2ab0c07f8130a
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-06 15:29:12 +0300

    add user_transaction_association_table to __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ("User", "Transaction", "Budget", "Goal", "Transaction", "Base", "Category")
+__all__ = (
+    "User",
+    "Transaction",
+    "Budget",
+    "Goal",
+    "Transaction",
+    "Base",
+    "Category",
+    "user_transaction_association_table",
+)
 
 from .user import User
 from .transaction import Transaction
@@ -6,3 +15,4 @@ from .budget import Budget
 from .goal import Goal
 from .base import Base
 from .category import Category
+from .user_transaction_association import user_transaction_association_table
