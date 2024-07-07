268da07fad11ff28822cf0fc2289581f5a098a05
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-08 02:18:05 +0300

    change order of the routers

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -9,10 +9,10 @@ from .user import router as user_router
 from .demo_auth import router as demo_auth_router
 
 router = APIRouter()
-router.include_router(budget_router)
-router.include_router(category_router)
-router.include_router(fake_data_router)
-router.include_router(goal_router)
-router.include_router(transaction_router)
 router.include_router(user_router)
+router.include_router(category_router)
+router.include_router(transaction_router)
+router.include_router(goal_router)
+router.include_router(budget_router)
+router.include_router(fake_data_router)
 router.include_router(demo_auth_router)
