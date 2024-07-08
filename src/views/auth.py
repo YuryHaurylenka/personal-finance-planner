3a85e0f83f8c81a82f4dc11e94653640823e593d
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-09 02:43:26 +0300

    ...

--- src/views/auth.py
+++ src/views/auth.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter, Depends
 
-from src.auth.helpers import get_current_user
 from src.schemas.user import User
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@router.get("/basic-auth-username/")
-async def basic_auth_username(auth_user: User = Depends(get_current_user)):
-    return {
-        "message": f"Hi, {auth_user.username}!",
-        "username": auth_user.username,
-    }
+# @router.get("/basic-auth-username/")
+# async def basic_auth_username(auth_user: User = Depends(get_current_user)):
+#     return {
+#         "message": f"Hi, {auth_user.username}!",
+#         "username": auth_user.username,
+#     }
