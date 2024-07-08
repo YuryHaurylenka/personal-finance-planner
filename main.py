da229866445a29b4fff48028eaf4bfdc0b4d1a51
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-09 02:43:07 +0300

    add dispose and redirect to docs

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from starlette.responses import RedirectResponse
 
+from src.core.db_helper import db_helper
 from src.views import router as router_v1
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # startup
     yield
+    # shutdown
+    await db_helper.dispose()
 
 
-app = FastAPI(title="personal-finance-planner", lifespan=lifespan)
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
 app.include_router(router_v1)
+
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
 
 
 if __name__ == "__main__":
