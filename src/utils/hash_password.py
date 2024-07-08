70e5b00ed3b2319c43b9ec5f872a3f551796027e
GavrilenkoYura <gshshaaha@gmail.com>
2024-07-08 20:15:27 +0300

    add verify_password func

--- src/utils/hash_password.py
+++ src/utils/hash_password.py
@@ -5,3 +5,7 @@ def hash_password(password: bytes) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password, salt)
     return hashed
+
+
+def verify_password(password: bytes, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(password, hashed_password)
